b0c0c948d4ded07a5e894f9ce9205199f6560776
Zach1993 <zachllee93@gmail.com>
2016-08-25 17:17:07 -0700

    Initial Commit

---
+++ EncryptionPractice1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  EncryptionPractice1
+//
+//  Created by zach lee on 8/25/16.
+//  Copyright © 2016 zach lee. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
